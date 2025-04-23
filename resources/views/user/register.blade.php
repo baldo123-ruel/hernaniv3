1e16d2110000d3a8c63f02dd44a0bb291bc283f6
Ruel Baldo <bebebaldo75@gmail.com>
2025-04-23 10:05:35 +0800

    Register update

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <link href="https://hernaniv3-production.up.railway.app/css/style.css" rel="stylesheet">
   <title>Register</title>
   <style>
     .step {
